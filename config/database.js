2cc15adac2a8c28d2ce8017a40a78874641a9881
chensational <achen.butler@gmail.com>
2016-11-05 18:17:45 -0500

    heroku update

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 // called by server.js by var configDB = requires('./config/database.js')
 
 module.exports = {
-	'url' : 'mongodb://heroku_stzbwk35:up7ofiq7vqjmb9062h05ibhsbv@ds139847.mlab.com:39847/heroku_stzbwk35'
+	'url' : 'mongodb://admin:newmeta@ds145667.mlab.com:45667/newmetadb'
 };
